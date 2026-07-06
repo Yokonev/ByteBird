52741905da19192b8334eb566f802183a91f7ee4
Yokonev <nathan.scohier@proton.me>
2026-07-06 23:36:26 +0200

    feat: Added CPU flag operations

--- src/cpu/reg_file.rs
+++ src/cpu/reg_file.rs
@@ -1,15 +1,10 @@
 // Representation of registers for the original Gameboy (DMG) model
-// Uses a vector of unsigned integers to represent the data of the registers
-// and enumerations to access them.
 
 const REG_COUNT: usize = 10;
+const FLAG_REG_STRIDE: usize = 3; //bits 0..3 are ignored for flags
 
-// Registers are already grouped to be more easily accessible together
-// Ex: Register A is 0, register F is 1, so register AF is register 0 and 0+1
-// Register B is 2, register C is 3, so register BC is register 2 and register 2+1...
-// This is used to facilitate reads and writes operations with 16 bits register ops
 #[derive(Clone, Copy)]
-enum DmaSimpleRegisters{
+enum DmgSimpleRegisters{
     A = 0, F = 1,
     B = 2, C = 3,
     D = 4, E = 5,
@@ -18,11 +13,19 @@ enum DmaSimpleRegisters{
 }
 
 #[derive(Clone, Copy)]
-enum DmaDoubleRegisters{
+enum DmgDoubleRegisters{
     AF = 0,
     BC = 1,
     DE = 2,
     HL = 3
+}
+
+#[derive(Clone, Copy)]
+enum DmgFlags{
+    ZERO = 4,
+    SUBTRACT = 3,
+    HCARRY = 2,
+    CARRY = 1
 }
 
 struct RegFile{
@@ -32,31 +35,46 @@ struct RegFile{
 impl RegFile {
 
     fn new() -> RegFile{
-        RegFile { registers: vec![0u8; REG_COUNT] }
+        RegFile { 
+            registers: vec![0u8; REG_COUNT]
+         }
     }
 
-    fn write_register(& mut self, target: DmaSimpleRegisters, value: u8) -> () {
+    fn write_register(& mut self, target: DmgSimpleRegisters, value: u8) -> () {
         self.registers[target as usize] = value;
     }
 
-    fn read_register(&self, target: DmaSimpleRegisters) -> u8 {
+    fn read_register(&self, target: DmgSimpleRegisters) -> u8 {
         self.registers[target as usize]
     }
 
-    fn write_double_register(& mut self, target: DmaDoubleRegisters, value: u16) -> () {
+    fn write_double_register(& mut self, target: DmgDoubleRegisters, value: u16) -> () {
         let upper: u8 = (value >> 8) as u8;
         let lower: u8 = (value & 0x00FF) as u8;
         let base_index: usize = (target as usize) * 2;
         self.registers[base_index] = lower;
         self.registers[base_index + 1] = upper;
-
     }
 
-    fn read_double_register(&self, target: DmaDoubleRegisters) -> u16 {
+    fn read_double_register(&self, target: DmgDoubleRegisters) -> u16 {
         let base_index: usize = (target as usize) * 2;
         let upper: u8 = self.registers[base_index + 1];
         let lower: u8 = self.registers[base_index];
         ((upper as u16) << 8) | (lower as u16)
+    }
+
+    // Reminder, the F register reads as follows: 0xZNHC0000
+    fn read_flag(&self, target: DmgFlags) -> bool {
+        (self.registers[DmgSimpleRegisters::F as usize] & (1 << (FLAG_REG_STRIDE + target as usize))) != 0
+    }
+
+    fn write_flag(& mut self, target: DmgFlags, value: bool) -> () {
+        let mask = 1u8 << (FLAG_REG_STRIDE + target as usize);
+
+        match value {
+            true  => (self.registers[DmgSimpleRegisters::F as usize]) |= mask,
+            false => (self.registers[DmgSimpleRegisters::F as usize]) &= !mask
+        };
     }
 }
 
@@ -65,16 +83,18 @@ mod tests {
 
     use super::*;
 
+    //Verifies that a new RegFile contains a vector of 10 zeroes
     #[test]
-    fn test_regfile_correctly_initialized() {
+    fn test_registers_correctly_initialized() {
         let reg_file: RegFile = RegFile::new();
-        assert_eq!(vec![0; 10], reg_file.registers)
+        assert_eq!(vec![0; REG_COUNT], reg_file.registers)
     }
 
+    //Verifies that the double reads and writes work as intended considering the little-endian representation
     #[test]
     fn test_read_write_double_register() {
         let value: u16 = 0xABCD;
-        let target: DmaDoubleRegisters = DmaDoubleRegisters::HL;
+        let target: DmgDoubleRegisters = DmgDoubleRegisters::HL;
         let mut reg_file: RegFile = RegFile::new();
         
         reg_file.write_double_register(target, value);
@@ -83,4 +103,22 @@ mod tests {
         assert_eq!(result, value)
     }
 
+    #[test]
+    fn test_modifying_flags_work() {
+        let mut reg_file: RegFile = RegFile::new();
+        let expected_one: u8 = 0b1111_0000;
+        let expected_two: u8 = 0b1001_0000;
+
+        reg_file.write_flag(DmgFlags::ZERO, true);
+        reg_file.write_flag(DmgFlags::SUBTRACT, true);
+        reg_file.write_flag(DmgFlags::HCARRY, true);
+        reg_file.write_flag(DmgFlags::CARRY, true);
+        
+        assert_eq!(expected_one, reg_file.read_register(DmgSimpleRegisters::F));
+
+        reg_file.write_flag(DmgFlags::SUBTRACT, false);
+        reg_file.write_flag(DmgFlags::HCARRY, false);
+
+        assert_eq!(expected_two, reg_file.read_register(DmgSimpleRegisters::F));
+    }
 }
